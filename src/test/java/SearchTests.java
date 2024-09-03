8d7be00b364e64c6c992eab4c1242f80d4cf4fce
Sarycheva Daria <104355434+DariaS2302@users.noreply.github.com>
2024-09-03 18:05:21 +0300

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -15,6 +15,6 @@ public class SearchTests {
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://ssselenide.org"));
+        $("[id=search]").shouldHave(text("https://selenide.org"));
     }
 }
